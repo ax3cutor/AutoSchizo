6e9b914fe3dd692299988553b3b3f4dea9de8170
ax3cutor <69725765+ax3cutor@users.noreply.github.com>
2021-03-27 21:30:51 +0000

    make it actually work

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import requests, yaml, feedparser, random, time, traceback, os, sys
+import requests, yaml, feedparser, random, time, traceback
 
 BASE_URL = 'https://ruqqus.com'
 SUBMIT_URL = f'{BASE_URL}/api/v1/submit'
@@ -6,11 +6,12 @@ REFRESH_URL = f'{BASE_URL}/oauth/grant'
 
 with open('config.yaml') as config_file:
 	config = yaml.safe_load(config_file)
+	#print(config)
 	ID = config['id']
-	access_token = config['access-token']
 	SECRET = config['secret']
 	REFRESH_TOKEN = config['refresh-token']
 	USER_AGENT = config['user-agent']
+	
 
 FEEDS = ['https://thegatewaypundit.com/feed/', 'https://bigleaguepolitics.com/feed/', 'https://dailywire.com/feeds/rss.xml']
 GUILDS = ['Conservatives', 'Politic', 'Politics', 'News']
@@ -23,6 +24,7 @@ def refresh_token():
 										     "client_secret": SECRET,
 										     "grant_type": "refresh",
 										     "refresh_token": REFRESH_TOKEN})
+	#update config with the valid token
 	with open('config.yaml') as config_file:
 		new_config = yaml.load(config_file)
 
@@ -31,58 +33,64 @@ def refresh_token():
 	with open('config.yaml', 'w') as config_file:
 		yaml.dump(new_config, config_file)
 
-	access_token = new_config['access-token']
 	print(f"new access token: {new_config['access-token']}")
 
-	#restart
-	print("restarting...")
-	os.execv(sys.argv[0], sys.argv)
+	print("token updated")
 
-while True:
-	try:
-		feed = random.choice(FEEDS)
-		guild = random.choice(GUILDS)
-
-		for entry in feedparser.parse(feed).entries:
-			text = f"{entry.link} {entry.title}"
-			with open('articles.txt', 'a') as f:
-				f.write(text + '\n')
-		else:
-			with open('articles.txt') as f:
-				lines = [l.rstrip() for l in f]
-				randent = random.choice(lines)
-				link = randent.split()[0]
-				title = randent.replace(link, '')
+def main():
+	print("starting main process...")
+	while True:
 		try:
-			post = requests.post(SUBMIT_URL, headers = {"Authorization": f"Bearer {access_token}",
-								    "User-Agent": USER_AGENT,
-								    "X-User-Type": "Bot"},
-							 data = {"title": title,
-								 "url": link,
-								 "board": guild})
-			post.raise_for_status()
-			print(f"submitted: +{guild} : {title} ({link})")
-			print("going to sleep for 1 min...")
-			time.sleep(60)
-		except requests.exceptions.HTTPError as e:
-			print(f"HTTP error {e.response.status_code} {e.response.reason}")
-			time.sleep(5)
-			if e.response.status_code == 401:
-				#refresh token
-				refresh_token()
-				time.sleep(5)
+			#load credentials
+			with open('config.yaml') as config_file:
+				config = yaml.safe_load(config_file)
+				access_token = config['access-token']
+				auth_header = f"Bearer {access_token}"
+
+			feed = random.choice(FEEDS)
+			guild = random.choice(GUILDS)
+
+			for entry in feedparser.parse(feed).entries:
+				text = f"{entry.link} {entry.title}"
+				with open('articles.txt', 'a') as f:
+					f.write(text + '\n')
 			else:
-				time.sleep(55)
-		finally:
-			#clear articles.txt
-			open("articles.txt", "r+").truncate(0)
-	except Exception: 
-		print(traceback.format_exc())
-		time.sleep(60)
-	except KeyboardInterrupt:
-		print("goodnight, bye bye... :(")
-		quit()
+				with open('articles.txt') as f:
+					lines = [l.rstrip() for l in f]
+					randent = random.choice(lines)
+					link = randent.split()[0]
+					title = randent.replace(link, '')
+			try:
+				print(auth_header)
+				post = requests.post(SUBMIT_URL, headers = {"Authorization": auth_header,
+									    "User-Agent": USER_AGENT,
+									    "X-User-Type": "Bot"},
+								 data = {"title": title,
+									 "url": link,
+									 "board": guild})
+				post.raise_for_status()
+				print(f"SUBMITTED TO: +{guild} (title: {title} | url: {link})")
+				print("going to sleep for 1 min...")
+				time.sleep(60)
+			except requests.exceptions.HTTPError as e:
+				print(f"HTTP error {e.response.status_code} {e.response.reason}")
+				time.sleep(5)
+				if e.response.status_code == 401:
+					#refresh token
+					refresh_token()
+					time.sleep(5)
+				else:
+					time.sleep(55)
+			finally:
+				#clear articles.txt
+				open("articles.txt", "r+").truncate(0)
+		except Exception: 
+			print(traceback.format_exc())
+			time.sleep(60)
+		except KeyboardInterrupt:
+			print("goodnight, bye bye... :(")
+			quit()
 
-
-
+if __name__ == '__main__':
+	main()
 
